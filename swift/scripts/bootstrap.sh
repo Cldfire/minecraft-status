5774ae1ebca306df0686d7c8898949f42747cff8
Jarek Samic <cldfire3@gmail.com>
2021-02-07 01:52:22 -0500

    Set up CI

---
+++ swift/scripts/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "checking for homebrew updates";
+brew update
+
+function install_current {
+    echo "trying to update $1"
+    brew upgrade $1 || brew install $1 || true
+    brew link $1
+}
+
+if [ -e "Mintfile" ]; then
+    install_current mint
+    mint bootstrap
+fi
