1dfeea6610450c8623abbc7fbccf56c7fed2df60
Jarek Samic <cldfire3@gmail.com>
2021-04-13 23:13:19 -0400

    Move identicon creation to separate module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,11 +12,11 @@ use std::{
 };
 
 use anyhow::{anyhow, Context};
-use identicon_rs::Identicon;
-use image::EncodableLayout;
+use identicon::{make_base64_identicon, IdenticonInput};
 use mcping_common::{Player, Players, ProtocolType, Response, Version};
 use serde::{Deserialize, Serialize};
 
+mod identicon;
 mod mcping_common;
 #[cfg(test)]
 mod tests;
@@ -288,51 +288,6 @@ fn mcping_get_status_wrapper(
     }
 
     mcping_common::get_status(address, timeout, protocol_type)
-}
-
-struct IdenticonInput<'a> {
-    protocol_type: ProtocolType,
-    address: &'a str,
-}
-
-impl<'a> IdenticonInput<'a> {
-    fn to_string(&self) -> String {
-        format!("{:?}{}", self.protocol_type, self.address)
-    }
-}
-
-fn make_base64_identicon(input: IdenticonInput) -> Option<String> {
-    let identicon = Identicon::new(input.to_string())
-        .size(9)
-        .unwrap()
-        .scale(54)
-        .unwrap()
-        .border(6)
-        .background_color((0, 0, 0));
-    let dynamic_image = identicon.generate_image();
-    let mut rgba_image = dynamic_image.to_rgba8();
-
-    // Replace the background color with transparency
-    //
-    // We handle the background in swiftui land so we can react to system theme
-    // changes
-    rgba_image
-        .pixels_mut()
-        .filter(|p| *p == &image::Rgba([0, 0, 0, 255]))
-        .for_each(|p| *p = image::Rgba([0, 0, 0, 0]));
-
-    let mut buffer = Vec::new();
-
-    image::png::PngEncoder::new(&mut buffer)
-        .encode(
-            rgba_image.as_bytes(),
-            rgba_image.width(),
-            rgba_image.height(),
-            image::ColorType::Rgba8,
-        )
-        .ok()?;
-
-    Some(base64::encode(&buffer))
 }
 
 /// The rusty version of what we need to get done.
